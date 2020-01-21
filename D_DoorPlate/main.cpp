9f7386161914766968fc67b3f75945d4d77cbdf4
killuahh <40869167+killuahh@users.noreply.github.com>
2020-01-21 17:27:03 +0100

    Database conn

--- D_DoorPlate/main.cpp
+++ D_DoorPlate/main.cpp
@@ -1,7 +1,98 @@
 
 #include <iostream>
+#include <windows.h>
+#include <sqlext.h>
+#include <sqltypes.h>
+#include <sql.h>
+
+using namespace std;
+
+void showSQLError(unsigned int handleType, const SQLHANDLE& handle)
+{
+	SQLCHAR SQLState[1024];
+	SQLCHAR message[1024];
+	if (SQL_SUCCESS == SQLGetDiagRec(handleType, handle, 1,SQLState, NULL, message, 1024, NULL))
+		// Returns the current values of multiple fields of a diagnostic record that contains error, warning, and status information
+		cout << "SQL driver message: " << message << "\nSQL state: " << SQLState << "." << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SQLHANDLE SQLEnvHandle = NULL;
+	SQLHANDLE SQLConnectionHandle = NULL;
+	SQLHANDLE SQLStatementHandle = NULL;
+	SQLRETURN retCode = 0;
+	char SQLQuery[] = "SELECT * FROM kunde";
+
+	do {
+		if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &SQLEnvHandle))
+			// Allocates the environment
+			break;
+
+		if (SQL_SUCCESS != SQLSetEnvAttr(SQLEnvHandle, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0))
+			// Sets attributes that govern aspects of environments
+			break;
+
+		if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_DBC, SQLEnvHandle, &SQLConnectionHandle))
+			// Allocates the connection
+			break;
+
+		if (SQL_SUCCESS != SQLSetConnectAttr(SQLConnectionHandle, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0))
+			// Sets attributes that govern aspects of connections
+			break;
+
+		SQLCHAR retConString[1024]; // Conection string
+		switch (SQLDriverConnect(SQLConnectionHandle, NULL, (SQLCHAR*)"DRIVER={SQL Server}; SERVER=localhost, 1433; DATABASE=test; UID=sa; PWD=test112;", SQL_NTS, retConString, 1024, NULL, SQL_DRIVER_NOPROMPT)) {
+			// Establishes connections to a driver and a data source
+		case SQL_SUCCESS:
+			break;
+		case SQL_SUCCESS_WITH_INFO:
+			break;
+		case SQL_NO_DATA_FOUND:
+			showSQLError(SQL_HANDLE_DBC, SQLConnectionHandle);
+			retCode = -1;
+			break;
+		case SQL_INVALID_HANDLE:
+			showSQLError(SQL_HANDLE_DBC, SQLConnectionHandle);
+			retCode = -1;
+			break;
+		case SQL_ERROR:
+			showSQLError(SQL_HANDLE_DBC, SQLConnectionHandle);
+			retCode = -1;
+			break;
+		default:
+			break;
+		}
+
+		if (retCode == -1)
+			break;
+
+		if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_STMT, SQLConnectionHandle, &SQLStatementHandle))
+			// Allocates the statement
+			break;
+
+		if (SQL_SUCCESS != SQLExecDirect(SQLStatementHandle, (SQLCHAR*)SQLQuery, SQL_NTS)) {
+			// Executes a preparable statement
+			showSQLError(SQL_HANDLE_STMT, SQLStatementHandle);
+			break;
+		}
+		else {
+			char name[256];
+			int id;
+			while (SQLFetch(SQLStatementHandle) == SQL_SUCCESS) {
+				// Fetches the next rowset of data from the result
+				SQLGetData(SQLStatementHandle, 1, SQL_C_DEFAULT, &id, sizeof(id), NULL);
+				SQLGetData(SQLStatementHandle, 2, SQL_C_DEFAULT, &name, sizeof(name), NULL);
+				// Retrieves data for a single column in the result set
+				cout << "id: " << id << " Name: " << name << endl;
+			}
+		}
+	} while (SQL_SUCCESS != SQLExecDirect(SQLStatementHandle, (SQLCHAR*)SQLQuery, SQL_NTS));
+
+	SQLFreeHandle(SQL_HANDLE_STMT, SQLStatementHandle);
+	SQLDisconnect(SQLConnectionHandle);
+	SQLFreeHandle(SQL_HANDLE_DBC, SQLConnectionHandle);
+	SQLFreeHandle(SQL_HANDLE_ENV, SQLEnvHandle);
+	// Frees the resources and disconnects
+	return 0;
 }
