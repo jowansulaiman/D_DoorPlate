3a4405c38bb489c3fd7b1b386a3a79c56e2d72ae
killuahh <40869167+killuahh@users.noreply.github.com>
2020-01-27 18:10:14 +0100

    Datenbankverbindung sowie Abfragen

---
+++ D_Database/DBConn.h
@@ -0,0 +1,37 @@
+#ifndef D_DBConn
+#define D_DBConn
+#include <iostream>
+#include <windows.h>
+#include <sqlext.h>
+#include <sqltypes.h>
+#include <sql.h>
+#include <string.h>
+
+
+class DBConn {
+private:
+	SQLCHAR* SQLState[1024];
+	SQLCHAR message[1024];
+	SQLHANDLE SQLEnvHandle;
+	SQLHANDLE SQLConnectionHandle;
+	SQLHANDLE SQLStatementHandle;
+	SQLRETURN retCode;
+	char SQLQuery[];
+public:
+	DBConn() {
+		SQLEnvHandle = NULL;
+		SQLConnectionHandle = NULL;
+		SQLStatementHandle = NULL;
+		retCode = 0;
+	}
+	SQLHANDLE getconn();
+	void statment();
+	void showSQLError(unsigned int handleType, const SQLHANDLE& handle);
+};
+
+
+
+
+
+
+#endif // !D_DBConn
---
+++ D_Database/DBmain.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+#include "DBConn.h"
+int main()
+{
+	DBConn db;
+	db.getconn();
+	db.statment();
+
+	return 0;
+
+}
+
