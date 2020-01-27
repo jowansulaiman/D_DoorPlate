7329103a7b2f81f807dddd4f2b673dc5023534cd
killuahh <40869167+killuahh@users.noreply.github.com>
2020-01-27 16:40:39 +0100

    DIe Datenbank

--- D_DoorPlate/DateTime.cpp
+++ D_DoorPlate/DateTime.cpp
@@ -1,9 +1,41 @@
 #include "DateTime.h"
-
+#include <iostream>
 bool Date::chec_Time() {
 	return true;
 }
 
-void Date::time() {
+void Date::_time() {
+
+  // Anzeige von Uhrzeit im Stil des Betriebssystems.
+    _strtime_s(tmpbuf, 128);
+    printf("OS time:\t%s\n", tmpbuf);
+ }
+
+
+void Date::_date() {
+    // Anzeige von Datum im Stil des Betriebssystems.
+    _strdate_s(tmpbuf, 128);
+    printf("date:\t %s", tmpbuf);
+    std::cout << std::endl;
+}
+void Date::_day() {
 
 }
+void Date::_month() {
+
+}
+void Date::_year() {
+
+}
+void Date::_hour() {
+
+}
+void Date::_min() {
+
+}
+
+
+
+
+
+
--- D_DoorPlate/DateTime.h
+++ D_DoorPlate/DateTime.h
@@ -1,13 +1,27 @@
 #ifndef D_DateTime
 #define D_DateTime
 
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/timeb.h>
+#include <string.h>
+
+
 class Date{
 protected:
 	int *min;
 	int * hou;
 	int *day, *month, *year;
-	
+
+	char tmpbuf[128], timebuf[26], ampm[4] = "AM";
+	time_t ltime;
+	struct _timeb tstruct;
+	struct tm today, gmt, xmas = { 0, 0, 12, 25, 11, 93 };
+	errno_t err;
 public:
+	Date() {  };
 	Date(int m, int h, int d, int mo, int y) {
 		 min, hou, day, month, year = new int;
 
@@ -20,7 +34,13 @@ public:
 	}
 
 	bool chec_Time();
-	void time();
+	void _time();
+	void _date();
+	void _day();
+	void _month();
+	void _year();
+	void _hour();
+	void _min();
 	friend class DateTime;
 };
 
@@ -28,6 +48,8 @@ public:
 private:
 	Date start;
 	Date end;
+ 
+
 public:
 	DateTime(int s, int m, int h, int d, int mo, int y,
 	       	 int e_s, int e_m, int e_h, int e_d, int e_mo, int e_y):start(m,  h,  d,  mo,  y), end(e_m,  e_h,  e_d,  e_mo,  e_y) {	}
@@ -36,7 +58,6 @@ public:
 	}
 	void str() {
 	
-
 	}
 
 
